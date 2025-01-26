4bd9d3cd0c2752dab60d23d8ef4b19949e1b9077
Turtel216 <papadimitrios216@gmail.com>
2025-01-27 01:20:19 +0200

    added users table

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE `products` (
   `image` varchar(255) NOT NULL,
   `category` varchar(255) NOT NULL,
   `description` text,
-  `rating` int NOT NULL,
+  `rating` decimal(3,2) NOT NULL,
   `num_reviews` int NOT NULL DEFAULT 0,
   `price` decimal(10,2) NOT NULL,
   `count_in_stock` int NOT NULL,
@@ -16,25 +16,32 @@ CREATE TABLE `products` (
   `updated_at` datetime
 );
 
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `is_admin` bool NOT NULL DEFAULT false
+);
+
 CREATE TABLE `orders` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `payment_method` varchar(255) NOT NULL,
   `tax_price` decimal(10,2) NOT NULL,
   `shipping_price` decimal(10,2) NOT NULL,
   `total_price` decimal(10,2) NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `order_items` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `order_id` int NOT NULL,
   `product_id` int NOT NULL,
-  `name` varchar(255) NOT NULL,
   `quantity` int NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `price` decimal(10,2) NOT NULL
+  `price` decimal(10,2) NOT NULL,
+  FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
 );
-
-ALTER TABLE `order_items` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
-ALTER TABLE `order_items` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
