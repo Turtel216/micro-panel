d8bd09d11c2b02c7c5fbaa8f3099e15a56661982
Turtel216 <papadimitrios216@gmail.com>
2025-01-28 22:22:19 +0200

    added product sql methods

---
+++ cmd/micropanel-api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	db "github.com/Turtel216/micro-panel/data"
+)
+
+func main() {
+	db, err := db.NewDatabase()
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+
+	defer db.Close()
+	log.Println("Successfully connected to database")
+}
---
+++ data/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type DB struct {
+	db *sqlx.DB
+}
+
+func NewDatabase() (*DB, error) {
+	db, err := sqlx.Open("mysql", "root:passowrd@tcp(localhost:3306)/micropanel?parseTime=true")
+	if err != nil {
+		return nil, fmt.Errorf("error opening dabase: %w", err)
+	}
+
+	return &DB{db: db}, err
+}
+
+func (d *DB) Close() error {
+	return d.db.Close()
+}
+
+func (d *DB) GetDB() *sqlx.DB {
+	return d.db
+}
