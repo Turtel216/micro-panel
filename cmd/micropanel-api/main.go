99c00944b568f2f3487f464a27c2a7bc49c41db1
Turtel216 <papadimitrios216@gmail.com>
2025-02-05 18:52:48 +0200

    added product routes

--- cmd/micropanel-api/main.go
+++ cmd/micropanel-api/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 
 	db "github.com/Turtel216/micro-panel/data"
+	"github.com/Turtel216/micro-panel/micropanel-api/handler"
+	"github.com/Turtel216/micro-panel/micropanel-api/server"
+	"github.com/Turtel216/micro-panel/micropanel-api/storer"
 )
 
 func main() {
@@ -15,6 +18,9 @@ func main() {
 	defer db.Close()
 	log.Println("Successfully connected to database")
 
-	//st := storer.NewMySQLStorer(db.GetDB())
-	//str := server.NewServer(st)
+	st := storer.NewMySQLStorer(db.GetDB())
+	srv := server.NewServer(st)
+	hdl := handler.NewHandler(srv)
+	handler.RegisterRoutes(hdl)
+	handler.Start(":8080")
 }
