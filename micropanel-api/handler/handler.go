aed75c369c9c52750656e9d13d0251fca4241794
Turtel216 <papadimitrios216@gmail.com>
2025-02-06 14:38:32 +0200

    added order endpoints

--- micropanel-api/handler/handler.go
+++ micropanel-api/handler/handler.go
@@ -128,3 +128,75 @@ func (h *handler) deleteProduct(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 }
+
+func (h *handler) createOrder(w http.ResponseWriter, r *http.Request) {
+	var o OrderReq
+	if err := json.NewDecoder(r.Body).Decode(&o); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	created, err := h.server.CreateOrder(h.ctx, toStorerOrder(o))
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	res := toOrderRes(created)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (h *handler) getOrder(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	i, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "error parsing ID", http.StatusBadRequest)
+		return
+	}
+
+	order, err := h.server.GetOrder(h.ctx, i)
+	if err != nil {
+		http.Error(w, "Error getting order", http.StatusInternalServerError)
+		return
+	}
+
+	res := toOrderRes(order)
+	w.Header().Set("Context-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (h *handler) listOrders(w http.ResponseWriter, r *http.Request) {
+	orders, err := h.server.ListOrder(h.ctx)
+	if err != nil {
+		http.Error(w, "Error listening orders", http.StatusInternalServerError)
+		return
+	}
+
+	var res []OrderRes
+	for _, p := range orders {
+		res = append(res, toOrderRes(&p))
+	}
+
+	w.Header().Set("Context-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (h *handler) deleteOrder(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	i, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "error parsing ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.server.DeleteOrder(h.ctx, i); err != nil {
+		http.Error(w, "Error deleting order", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
--- micropanel-api/handler/types.go
+++ micropanel-api/handler/types.go
@@ -27,3 +27,33 @@ type ProductRes struct {
 	CreatedAt    time.Time  `json:"created_at"`
 	UpdatedAt    *time.Time `json:"updated_at"`
 }
+
+type OrderItem struct {
+	Name      string  `json:"name"`
+	Quantity  int64   `json:"quantity"`
+	Image     string  `json:"image"`
+	Price     float32 `json:"price"`
+	ProductID int64   `json:"product_id"`
+}
+
+type OrderReq struct {
+	ID            int64       `json:"id"`
+	Items         []OrderItem `json:"items"`
+	PaymentMethod string      `json:"payment_method"`
+	TaxPrice      float32     `json:"tax_price"`
+	ShippingPrice float32     `json:"shipping_price"`
+	TotalPrice    float32     `json:"total_price"`
+	Status        string      `json:"status"`
+}
+
+type OrderRes struct {
+	ID            int64       `json:"id"`
+	Items         []OrderItem `json:"items"`
+	PaymentMethod string      `json:"payment_method"`
+	TaxPrice      float32     `json:"tax_price"`
+	ShippingPrice float32     `json:"shipping_price"`
+	TotalPrice    float32     `json:"total_price"`
+	Status        string      `json:"status"`
+	CreatedAt     time.Time   `json:"created_at"`
+	UpdatedAt     *time.Time  `json:"updated_at"`
+}
